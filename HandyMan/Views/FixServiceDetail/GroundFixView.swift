78fca242a08a2c6d0f100bbf99e57960c773db5e
GARYW <qibiaowe@gmail.com>
2022-01-06 00:12:58 -0500

    For Service View

---
+++ HandyMan/Views/FixServiceDetail/GroundFixView.swift
@@ -0,0 +1,20 @@
+//
+//  GroundFixView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/5/22.
+//
+
+import SwiftUI
+
+struct GroundFixView: View {
+    var body: some View {
+        Text("Ground Fix View")
+    }
+}
+
+struct GroundFixView_Previews: PreviewProvider {
+    static var previews: some View {
+        GroundFixView()
+    }
+}
