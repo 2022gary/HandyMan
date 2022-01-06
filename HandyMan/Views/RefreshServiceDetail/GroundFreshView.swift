1798d7f403193249c2b7d7bee980f6d676ca0334
GARYW <qibiaowe@gmail.com>
2022-01-06 00:16:47 -0500

    For Service View

---
+++ HandyMan/Views/RefreshServiceDetail/GroundFreshView.swift
@@ -0,0 +1,20 @@
+//
+//  GroundFreshView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/5/22.
+//
+
+import SwiftUI
+
+struct GroundFreshView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GroundFreshView_Previews: PreviewProvider {
+    static var previews: some View {
+        GroundFreshView()
+    }
+}
