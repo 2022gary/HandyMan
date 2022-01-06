909b6ea0ce421bd0e6ddce09ba81685e608c97be
GARYW <qibiaowe@gmail.com>
2022-01-06 00:25:29 -0500

    View

---
+++ HandyMan/Views/InstallServiceDetail/DoorLockInstallView.swift
@@ -0,0 +1,17 @@
+//
+//  DoorLockInstallView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/6/22.
+//
+
+import SwiftUI
+
+struct DoorLockInstallView: View {
+    var body: some View {
+        Text("Door Lock View")
+    }
+}
+
+
+
