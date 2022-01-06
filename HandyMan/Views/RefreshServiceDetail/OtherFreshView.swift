1a6f0b9b18dbe46426ca186f7fff8c5ab48c09a6
GARYW <qibiaowe@gmail.com>
2022-01-06 00:16:23 -0500

    For Service View

---
+++ HandyMan/Views/RefreshServiceDetail/OtherFreshView.swift
@@ -0,0 +1,20 @@
+//
+//  OtherFreshView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/5/22.
+//
+
+import SwiftUI
+
+struct OtherFreshView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OtherFreshView_Previews: PreviewProvider {
+    static var previews: some View {
+        OtherFreshView()
+    }
+}
