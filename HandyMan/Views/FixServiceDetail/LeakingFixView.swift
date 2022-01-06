e81a6fb0d39f53af1a49461f0fcb4aabdba52895
GARYW <qibiaowe@gmail.com>
2022-01-06 00:13:07 -0500

    For Service View

---
+++ HandyMan/Views/FixServiceDetail/LeakingFixView.swift
@@ -0,0 +1,20 @@
+//
+//  LeakingFixView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/5/22.
+//
+
+import SwiftUI
+
+struct LeakingFixView: View {
+    var body: some View {
+        Text("Leaking Fix")
+    }
+}
+
+struct LeakingFixView_Previews: PreviewProvider {
+    static var previews: some View {
+        LeakingFixView()
+    }
+}
