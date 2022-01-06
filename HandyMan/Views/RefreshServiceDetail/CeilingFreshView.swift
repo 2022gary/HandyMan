6bb79d7701859fe1733bead3843d6c0e85e04be5
GARYW <qibiaowe@gmail.com>
2022-01-06 00:15:36 -0500

    For Service View

---
+++ HandyMan/Views/RefreshServiceDetail/CeilingFreshView.swift
@@ -0,0 +1,20 @@
+//
+//  CeilingFreshView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/5/22.
+//
+
+import SwiftUI
+
+struct CeilingFreshView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CeilingFreshView_Previews: PreviewProvider {
+    static var previews: some View {
+        CeilingFreshView()
+    }
+}
