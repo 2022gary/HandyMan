787f63ec61dddda30b4611b2d41275fc0a69e0ba
GARYW <qibiaowe@gmail.com>
2022-01-06 00:17:50 -0500

    For Service View

---
+++ HandyMan/Views/InstallServiceDetail/FloorInstallView.swift
@@ -0,0 +1,20 @@
+//
+//  FloorInstallView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/5/22.
+//
+
+import SwiftUI
+
+struct FloorInstallView: View {
+    var body: some View {
+        Text("Floor View")
+    }
+}
+
+struct FloorInstallView_Previews: PreviewProvider {
+    static var previews: some View {
+        FloorInstallView()
+    }
+}
