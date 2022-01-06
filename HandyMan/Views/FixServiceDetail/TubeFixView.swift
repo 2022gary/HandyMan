3447b3b6b57a882d2f84d18645ac52100eb5c180
GARYW <qibiaowe@gmail.com>
2022-01-06 00:13:31 -0500

    For Service View

---
+++ HandyMan/Views/FixServiceDetail/TubeFixView.swift
@@ -0,0 +1,20 @@
+//
+//  TubeFixView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/5/22.
+//
+
+import SwiftUI
+
+struct TubeFixView: View {
+    var body: some View {
+        Text("TubeFix View")
+    }
+}
+
+struct TubeFixView_Previews: PreviewProvider {
+    static var previews: some View {
+        TubeFixView()
+    }
+}
