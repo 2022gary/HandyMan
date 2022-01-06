f26d3f56cf284eff9cb5f229b2f8e0691812c907
GARYW <qibiaowe@gmail.com>
2022-01-06 00:21:16 -0500

    View

---
+++ HandyMan/Views/FixServiceDetail/DoorWindowFixView.swift
@@ -0,0 +1,20 @@
+//
+//  DoorWindowFixView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/6/22.
+//
+
+import SwiftUI
+
+struct DoorWindowFixView: View {
+    var body: some View {
+        Text("Door Fix View")
+    }
+}
+
+struct DoorWindowFixView_Previews: PreviewProvider {
+    static var previews: some View {
+        DoorWindowFixView()
+    }
+}
