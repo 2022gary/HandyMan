3b25aded38d0862c9c08ceac0891f80fd7995a71
GARYW <qibiaowe@gmail.com>
2022-01-06 00:15:46 -0500

    For Service View

---
+++ HandyMan/Views/RefreshServiceDetail/DoorFreshView.swift
@@ -0,0 +1,20 @@
+//
+//  DoorFreshView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/5/22.
+//
+
+import SwiftUI
+
+struct DoorFreshView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DoorFreshView_Previews: PreviewProvider {
+    static var previews: some View {
+        DoorFreshView()
+    }
+}
