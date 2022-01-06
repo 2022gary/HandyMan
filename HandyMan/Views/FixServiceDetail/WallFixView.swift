1bebe44330d6a7bf5b5aa7b691846d58c57d2417
GARYW <qibiaowe@gmail.com>
2022-01-06 00:21:38 -0500

    viEW

---
+++ HandyMan/Views/FixServiceDetail/WallFixView.swift
@@ -0,0 +1,20 @@
+//
+//  WallFixView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/6/22.
+//
+
+import SwiftUI
+
+struct WallFixView: View {
+    var body: some View {
+        Text("WallFixView")
+    }
+}
+
+struct WallFixView_Previews: PreviewProvider {
+    static var previews: some View {
+        WallFixView()
+    }
+}
