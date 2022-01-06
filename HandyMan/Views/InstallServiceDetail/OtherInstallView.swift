5a72d34cb76f4a3ddc57db7cb15a92a23547fe50
GARYW <qibiaowe@gmail.com>
2022-01-06 00:17:15 -0500

    For Service View

---
+++ HandyMan/Views/InstallServiceDetail/OtherInstallView.swift
@@ -0,0 +1,20 @@
+//
+//  OtherInstallView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/5/22.
+//
+
+import SwiftUI
+
+struct OtherInstallView: View {
+    var body: some View {
+        Text("Other View")
+    }
+}
+
+struct OtherInstallView_Previews: PreviewProvider {
+    static var previews: some View {
+        OtherInstallView()
+    }
+}
