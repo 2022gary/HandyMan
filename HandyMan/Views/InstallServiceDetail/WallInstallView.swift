2fe197fa0229f988be47c9eca186801c8e04c2ff
GARYW <qibiaowe@gmail.com>
2022-01-06 00:25:37 -0500

    View

---
+++ HandyMan/Views/InstallServiceDetail/WallInstallView.swift
@@ -0,0 +1,20 @@
+//
+//  WallInstallView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/6/22.
+//
+
+import SwiftUI
+
+struct WallInstallView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WallInstallView_Previews: PreviewProvider {
+    static var previews: some View {
+        WallInstallView()
+    }
+}
