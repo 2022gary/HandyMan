b4a606e2d9477df407e1b018a3bd52c3cd3a6c3e
GARYW <qibiaowe@gmail.com>
2022-01-06 00:13:23 -0500

    For Service View

---
+++ HandyMan/Views/FixServiceDetail/OtherView.swift
@@ -0,0 +1,20 @@
+//
+//  OtherView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/5/22.
+//
+
+import SwiftUI
+
+struct OtherView: View {
+    var body: some View {
+        Text("Other View")
+    }
+}
+
+struct OtherView_Previews: PreviewProvider {
+    static var previews: some View {
+        OtherView()
+    }
+}
