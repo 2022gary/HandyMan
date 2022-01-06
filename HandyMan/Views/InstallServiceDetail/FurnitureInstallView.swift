76cf1e19847c4cb1684194fb3a5645baa492b4f5
GARYW <qibiaowe@gmail.com>
2022-01-06 00:18:01 -0500

    For Service View

---
+++ HandyMan/Views/InstallServiceDetail/FurnitureInstallView.swift
@@ -0,0 +1,20 @@
+//
+//  FurnitureInstallView.swift
+//  HandyMan
+//
+//  Created by Gary Weng on 1/5/22.
+//
+
+import SwiftUI
+
+struct FurnitureInstallView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FurnitureInstallView_Previews: PreviewProvider {
+    static var previews: some View {
+        FurnitureInstallView()
+    }
+}
