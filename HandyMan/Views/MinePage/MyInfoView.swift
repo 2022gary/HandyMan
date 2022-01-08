060bb9ce4f3ddd226c99b95f012d92cb84cc45a6
GARYW <qibiaowe@gmail.com>
2022-01-07 23:01:43 -0500

    info page update

--- HandyMan/Views/MinePage/MyInfoView.swift
+++ HandyMan/Views/MinePage/MyInfoView.swift
@@ -8,10 +8,31 @@
 import SwiftUI
 
 struct MyInfoView: View {
+    @State private var rowCount: Int = 0;
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader{geometry in
+            VStack(alignment: .center) {
+                List {
+                    ForEach(0 ... 3, id: \.self) {index in
+                        HStack {
+                            Text("账号:")
+                            Spacer()
+                            Text("917-234-7685")
+                            Text("修改")
+                                .foregroundColor(.blue)
+                        }.listRowSeparator(.hidden)
+                    }
+                    Text("汉迪曼提供最专业的服务")
+                        .foregroundColor(.gray)
+                        .font(.caption)
+                        .frame(width: geometry.size.width/1.1)
+                }
+                .listStyle(.plain)
+            }
+        }
     }
 }
+
 
 struct MyInfoView_Previews: PreviewProvider {
     static var previews: some View {
