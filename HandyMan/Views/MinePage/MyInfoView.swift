b0e9c84f47dccab2ed024cce7c80ef1ee8c41e12
origin <qibiaowe@gmail.com>
2022-01-11 00:59:58 -0500

    update my info page view in mine page

--- HandyMan/Views/MinePage/MyInfoView.swift
+++ HandyMan/Views/MinePage/MyInfoView.swift
@@ -8,28 +8,91 @@
 import SwiftUI
 
 struct MyInfoView: View {
-    @State private var rowCount: Int = 0;
+    @State var userName: String = "David Louis"
+    @State var userPhoneNum: String = "646-598-6666"
+    @State var addressStree: String = "8888 65th St"
+    @State var addressCity: String = "New York"
+    @State var addressState: String = "NY"
+    @State var addressZip: String = "11011"
+    @State var userEmail: String = "abc@gmail.com"
+    
     var body: some View {
-        GeometryReader{geometry in
+      
             VStack(alignment: .center) {
-                List {
-                    ForEach(0 ... 3, id: \.self) {index in
-                        HStack {
-                            Text("账号:")
-                            Spacer()
-                            Text("917-234-7685")
-                            Text("修改")
-                                .foregroundColor(.blue)
-                        }.listRowSeparator(.hidden)
+              
+                CellInfo(infoName: "称呼 *", textFieldText: "你的姓名", textFieldValue: userName)
+                
+                CellInfo(infoName: "电话号码 *", textFieldText: "你的电话号码", textFieldValue: userPhoneNum)
+                
+                VStack(alignment: .leading, spacing: 5){
+                    
+                    
+                    Text("地址 *")
+                        .foregroundColor(.secondary)
+                        .font(.headline)
+                    HStack {
+                        Text("门牌: ")
+                            .font(.title2)
+                        TextField("Your Stree", text: $addressStree)
+                            .font(.title2)
+                            .padding(.all, 5)
+                            .background(Color.secondary.opacity(0.2))
+                            .cornerRadius(10)
                     }
-                    Text("汉迪曼提供最专业的服务")
-                        .foregroundColor(.gray)
-                        .font(.caption)
-                        .frame(width: geometry.size.width/1.1)
+                    
+                    HStack {
+                        Text("城市: ")
+                            .font(.title2)
+                        TextField("Your City", text: $addressCity)
+                            .font(.title2)
+                            .padding(.all, 5)
+                            .background(Color.secondary.opacity(0.2))
+                            .cornerRadius(10)
+                        
+                        Text("州: ")
+                            .font(.title2)
+                        TextField("Your State", text: $addressState)
+                            .font(.title2)
+                            .padding(.all, 5)
+                            .background(Color.secondary.opacity(0.2))
+                            .cornerRadius(10)
+                    }
+                    
+                    HStack {
+                        Text("邮编: ")
+                            .font(.title2)
+                        TextField("Your Zip Code", text: $addressZip)
+                            .font(.title2)
+                            .padding(.all, 5)
+                            .background(Color.secondary.opacity(0.2))
+                            .cornerRadius(10)
+                    }
                 }
-                .listStyle(.plain)
-            }
+                .padding()
+                
+                CellInfo(infoName: "邮箱", textFieldText: "你的邮箱地址", textFieldValue: userEmail)
+                
+                
+                Button(action: {}) {
+                    Text("保存更新")
+                        .foregroundColor(.white)
+    //                    .font(.title2)
+                        .font(.system(size: 25))
+                        .fontWeight(.bold)
+                        .padding()
+                        .padding(.horizontal, 120)
+                        .background(Color.red)
+                        .clipShape(Capsule())
+                    
+                }
+                .padding()
+                
+                Spacer()
+                Text("汉迪曼提供最专业的服务")
+                    .foregroundColor(.gray)
+                    .font(.caption)
         }
+            
     }
 }
 
@@ -37,5 +100,27 @@ struct MyInfoView: View {
 struct MyInfoView_Previews: PreviewProvider {
     static var previews: some View {
         MyInfoView()
+    }
+}
+
+struct CellInfo: View {
+    var infoName: String
+    var textFieldText: String
+    @State var textFieldValue: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5){
+            
+            Text(infoName)
+                .foregroundColor(.secondary)
+                .font(.headline)
+            TextField(textFieldText, text: $textFieldValue)
+                .font(.title)
+                .padding()
+                .background(Color.secondary.opacity(0.2))
+                .cornerRadius(10)
+            
+        }
+        .padding()
     }
 }
